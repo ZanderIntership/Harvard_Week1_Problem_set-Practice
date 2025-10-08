d3aadcccfca3771fe60c52c9ed626ea3c1a00c09
ZanderIntership <ZanderWepener5@gmail.com>
2025-10-08 14:09:19 +0200

    Create Problem_Set1_1.2.c

---
+++ Problem_Set1_1.2.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);  // ensure valid height
+
+    for (int i = 0; i < height; i++)
+    {
+        // Print spaces
+        for (int j = 0; j < height - i - 1; j++)
+        {
+            printf(" ");
+        }
+
+        // Print blocks
+        for (int k = 0; k <= i; k++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+}
